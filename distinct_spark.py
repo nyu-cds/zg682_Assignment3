9df5b15ce1a6cae992f06487f4e9f34a97c549d4
GJGao <zg682@nyu.edu>
2017-05-07 19:27:04 -0400

    assignment 13-1 distinct spark

--- distinct_spark.py
+++ distinct_spark.py
@@ -7,13 +7,12 @@ def splitter(line):
         line = re.sub(r'^\W+|\W+$', '', line)
 	return map(str.lower, re.split(r'\W+', line))
 
-#using mapreduce concept
+#using mapreduce concept to count the distinct number of words
 if __name__ == '__main__':
         sc = SparkContext("local", "distinct_wordcount")
 	text = sc.textFile('pg2701.txt')#read file
 	words = text.flatMap(splitter)#splitter function on every line
         words_mapped = words.map(lambda x: (x,1))#playing map part
-        sorted_map = words_mapped.sortByKey()
-	counts = sorted_map.reduceByKey(lambda x,y:1)#playing reduce part
-	counts = words_mapped.count()
-	print("Distinct Words:", counts)
+	counts = words_mapped.reduceByKey(lambda x,y:1)#playing reduce part and then count
+	c = counts.count()
+	print("Distinct Words:", c)
