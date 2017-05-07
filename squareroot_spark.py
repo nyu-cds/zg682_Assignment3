fbeb548c0f65200c6f47d2e030539a93955cbca4
GJGao <zg682@nyu.edu>
2017-05-07 19:18:36 -0400

    assignment 13-3

---
+++ squareroot_spark.py
@@ -0,0 +1,11 @@
+from pyspark import SparkContext
+from operator import add
+from math import sqrt
+
+if __name__ == '__main__':
+    sc = SparkContext("local", "squareroot")    
+    # Create an RDD of numbers from 1 to 1,000
+    sqrts = sc.parallelize(range(1, 1001)).map(sqrt)
+    # Compute the average of the square roots
+    avg = sqrts.fold(0, add)/sqrts.count()
+    print(avg)
